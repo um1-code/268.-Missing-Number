5542cac208c4439fba8532f75414e9d143e18488
um1-code <umlat.at104@gmail.com>
2025-07-18 17:03:26 +0100

    Update 268. Missing Number.ts

---
+++ 268. Missing Number.ts
@@ -0,0 +1,7 @@
+function missingNumber(nums: number[]): number {
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = nums.reduce((sum, num) => sum + num, 0);
+
+  return expectedSum - actualSum;
+}
